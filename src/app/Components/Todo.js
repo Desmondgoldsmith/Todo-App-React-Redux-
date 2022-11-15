1f3b1c5366189ec2c976ec63e063e2817c7e687a
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-11-15 18:01:33 +0000

    THIS IS TODO APP 2.0 🌟m

--- src/app/Components/Todo.js
+++ src/app/Components/Todo.js
@@ -1,13 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './todo.css'
 
 function Todo() {
+    const [todo,setTodo] = useState("")
   return (
     <div className='Todo'>
     <form className='form_cls'>
-        <input className='input_cls'></input>
+        <input className='input_cls' ></input>
         <button className='btn_cls'>Add Todo</button>
-
     </form>
     </div>
   )
