59d6d710882ad7d82e5f2cdddf91733e5a961bea
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-11-16 01:49:29 +0000

    Todo App with REDUX  🌟

---
+++ src/app/Components/Todocontent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './todocontent.css'
+
+function Todocontent() {
+  return (
+    <div className='todo_c'>
+    Todocontent
+    </div>
+  )
+}
+
+export default Todocontent
