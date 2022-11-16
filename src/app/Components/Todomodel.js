f14787de2cd37d6de828cc88a4f14e1a41c8ec3d
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-11-16 02:14:28 +0000

    Todo App with REDUX  🌟

--- src/app/Components/Todomodel.js
+++ src/app/Components/Todomodel.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import './todoModel.css'
+import CloseIcon from '@mui/icons-material/Close';
 
 function Todomodel() {
   return (
     <div className='model'>
     <div className='container'>
      <div className = 'close_button'>
-
+       <CloseIcon />
      </div>
     <h1>Todomodel</h1>
     </div>
