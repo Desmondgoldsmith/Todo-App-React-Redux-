4843c997185e51a1607748b6b6356dab604df226
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-11-17 22:01:21 +0000

    Todo App with REDUX  🌟

--- src/Slices/todoSlice.js
+++ src/Slices/todoSlice.js
@@ -4,8 +4,10 @@ const getinitialTodo = () => {
     //getting todo values from local storage
     const localtodoList = window.localStorage.getItem('todoList')
     if(localtodoList){
-        
+        return JSON.parse(localtodoList)
     }
+    //basically what i'm doing here is that im getting the todos values from the local storage,
+    //once i get the values, i parse them to JSON  fromat.
 } 
 const initialValue = {
     //getting the todoList values from the local storage
