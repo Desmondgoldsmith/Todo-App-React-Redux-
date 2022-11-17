802ec0ff93d2565c3d4ca5da0503fabd17bad5dc
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-11-17 21:51:55 +0000

    Todo App with REDUX  🌟

--- src/Slices/todoSlice.js
+++ src/Slices/todoSlice.js
@@ -1,7 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const getinitialTodo = () => {
+    
+} 
 const initialValue = {
     //getting the todoList values from the local storage
-    //
+    //a function to get the todo values 
     todoList: getinitialTodo()
 }
