cf92190b5e6eb4428db22b4995234d30be1ae369
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-11-18 02:02:38 +0000

    Todo App with REDUX  🌟

--- src/app/Components/Todomodel.js
+++ src/app/Components/Todomodel.js
@@ -17,7 +17,7 @@ function Todomodel({openModel,setopenModel}) { //distructure the props obtained 
           id: uuid(),
           title,
           status,
-          time : new Date.toLocalDateString()
+          time : new Date().toLocaleDateString(),
         }))
       }
       console.log(title,status)
