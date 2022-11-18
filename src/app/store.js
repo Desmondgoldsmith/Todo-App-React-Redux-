4eb5fd75b97712bfcb1e44a0eddb9f78801d1acc
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-11-18 01:51:56 +0000

    Todo App with REDUX  🌟

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
+import todoReducer from "../Slices/todoSlice"
 export const store = configureStore({
   reducer: {
+    todo : todoReducer,
   },
 });
