a3d9dfa36e04cf8682864f699e2e690c43d82b5c
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-11-18 15:02:14 +0000

    TODO APP WITH REACT + REDUX

---
+++ src/app/Components/individualTodos.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function IndividualTodos() {
+  return (
+    <div className='todoItem'>
+    <div className='todoDetails'>
+            individualTodos
+
+    </div>
+    </div>
+  )
+}
+
+export default IndividualTodos
