5b5f9a77501994f2e8d315872bd3e2338adbb39e
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-11-18 14:51:09 +0000

    TODO APP WITH REACT + REDUX

--- src/app/Components/Todocontent.js
+++ src/app/Components/Todocontent.js
@@ -5,7 +5,8 @@ import './todocontent.css'
 function Todocontent() {
   const todoList = useSelector((state) => state.todo.todoList)
   //sorting the todos based on theit time
-  const sortedTodo = [...todoList]
+  const sortedTodo = [...todoList] //getting a copy of the todos array
+  sortedTodo.sort((a,b)=>)
   return (
     <div className='todo_c'>
     Todocontent
