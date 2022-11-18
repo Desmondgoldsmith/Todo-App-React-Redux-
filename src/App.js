582aeff85ca50c29b2b17bcdbc04a1a46c6744e1
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-11-18 02:19:43 +0000

    Adding Videos 💥

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import './App.css';
 import Todo from './app/Components/Todo';
 import Todocontent from './app/Components/Todocontent';
-
+import {Toastr} from 'reactjs-toastr/lib/react-toast';
 function App() {
   return (
+    <>
     <div className="App">
      <Todo />
      <Todocontent/>
       </div>
+     <Toastr/>
+    </>
   );
 }
 
