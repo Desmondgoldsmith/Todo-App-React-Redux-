749e376b22097d463ba4710c2256ff7a4f71ae26
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-11-21 00:37:43 +0000

    TODO APP WITH REACT + REDUX

--- src/app/Components/individualTodos.js
+++ src/app/Components/individualTodos.js
@@ -1,6 +1,8 @@
 // import { format } from 'date-fns'
 import React,{useState} from 'react'
 import './individualTodos.css'
+import './todoModel.css'
+
 import { MdDelete } from "react-icons/md";
 import { MdModeEdit } from "react-icons/md";
 import { useDispatch } from 'react-redux';
