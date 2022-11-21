bde88dbf97d13f2edfe7116ca0bbccfe4af2ae51
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-11-21 01:28:54 +0000

    TODO APP WITH REACT + REDUX

--- src/Slices/todoSlice.js
+++ src/Slices/todoSlice.js
@@ -52,7 +52,11 @@ export const todoSlice = createSlice({
         const todoList = window.localStorage.getItem('todoList')
         if(todoList){
             const todoListArray = JSON.parse(todoList)
-            todoListArray.forEach((todo,index)=> {}
+            todoListArray.forEach((todo,index)=> {
+                if(todoList.id === action.payload.id){
+                    
+                }
+            })
 
 
         }
