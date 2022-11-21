a1e083139b4dee6a08665d88b933b82df5030170
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-11-21 01:32:07 +0000

    TODO APP WITH REACT + REDUX

--- src/Slices/todoSlice.js
+++ src/Slices/todoSlice.js
@@ -54,6 +54,7 @@ export const todoSlice = createSlice({
             const todoListArray = JSON.parse(todoList)
             todoListArray.forEach((todo,index)=> {
                 if(todoList.id === action.payload.id){
+                    todo.title = action.payload.title
                     todo.status = action.payload.status
                 }
             })
